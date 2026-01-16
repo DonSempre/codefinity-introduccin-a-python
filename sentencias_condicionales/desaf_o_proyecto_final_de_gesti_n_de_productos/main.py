91bcf5c6812a57326bd6ace9f5a8c5d8efe26c36
DonSempre <>
2026-01-16 08:23:30 +0000

    Desafío: Proyecto Final de Gestión de Productos

---
+++ sentencias_condicionales/desaf_o_proyecto_final_de_gesti_n_de_productos/main.py
@@ -0,0 +1,15 @@
+days_until_expiration = 5
+stock_level = 60
+product_type = "Perishable"
+
+if product_type == "Perishable":
+    if days_until_expiration <= 3 and stock_level > 50:
+        print("30% discount applied")
+    elif 4 <= days_until_expiration <= 6 and stock_level > 50:
+        print("20% discount applied")
+    elif days_until_expiration > 6 and stock_level <= 50:
+        print("10% discount applied")
+    else:
+        print("No special discount")
+else:
+    print("No discount available for non-perishable items.")
